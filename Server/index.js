e1f86c9d6497feb64b6c8d19fb37026db2bc29c6
Elad Dahan <eladdahan@Elads-MacBook-Air.local>
2024-09-20 12:24:25 +0300

    Client folder init + server changes

--- Server/index.js
+++ Server/index.js
@@ -12,6 +12,19 @@ app.get('/', (req, res) => {
     res.send('Server is running!');
 });
 
+// Static list of code blocks (replace with DB later)
+const codeBlocks = [
+    { id: 1, title: 'Async Case', template: '/* Your async code here */', solution: '/* Expected solution */' },
+    { id: 2, title: 'Promise Case', template: '/* Your promise code here */', solution: '/* Expected solution */' },
+    { id: 3, title: 'Callback Case', template: '/* Your callback code here */', solution: '/* Expected solution */' },
+    { id: 4, title: 'Fetch API Case', template: '/* Your fetch API code here */', solution: '/* Expected solution */' },
+];
+
+// API endpoint to get code blocks
+app.get('/api/codeblocks', (req, res) => {
+    res.json(codeBlocks);
+});
+
 // Start the server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
